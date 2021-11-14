68fd381275b5476f03ce97db2c06dfcb74b433dc
ohoadit <aditshah8@gmail.com>
2021-11-14 05:21:09 -0600

    complete basic statistics

--- app.py
+++ app.py
@@ -13,33 +13,42 @@ import datetime as dt
 files = os.listdir('preprocessed')
 # print(files)
 
-companies = ['Capital One', 'Bank of America', 'JP Morgan Chase', 'Goldman Sachs', 'Morgan Stanley']
+companies = ['Bank of America', 'Capital One', 'JP Morgan Chase', 'Goldman Sachs',  'Morgan Stanley']
 
-def calc_change(predicted_val, previous_close):
-    return (predicted_val - previous_close) / 100
+# def calc_change(predicted_val, previous_close):
+#     return (predicted_val - previous_close) / 100
 
 def main():
 
     st.sidebar.title('Select Asset Type')
 
-    options = ["Stocks","Crypto"]
+    options = ["Top picks", "Stocks", "Crypto"]
 
     choice = st.sidebar.selectbox("Select one", options)
     
     if choice == "Stocks":
         for i in range(len(files)):
-            df = pd.read_csv(f"preprocessed/{files[i]}")
-            st.subheader(companies[i])
-            selected = st.checkbox('Show graph', key = companies[i])
-            if (selected):
-                fig  = px.line(df, x="Date", y="Close",title=companies[i])
-                st.plotly_chart(fig, use_container_width=True)
-                st.table(df[:20])
-        st.metric('Temperature', 42, -2.56)
+            try:
+                df = pd.read_csv(f"preprocessed/{files[i]}")
+                stats = pd.read_csv(f"output/{files[i]}", index_col = 0)
+                st.subheader(companies[i])
+                st.dataframe(stats)
+                selected = st.checkbox('Show graph', key = companies[i])
+                if (selected):
+                    fig  = px.line(df, x="Date", y="Close",title=companies[i])
+                    st.plotly_chart(fig, use_container_width=True)
+                    st.table(df[:20])
+            except:
+                print('Error')
+
+    # if choice == "Top picks":
+    # Print the top 3 recommended stocks
+
+        # st.metric('Temperature', 42, -2.56)
         
         # if st.checkbox("Show Technical Analysis",False):
 
-        #     st.subheader("checkbox for techical indicator") 
+    # st.subheader("checkbox for techical indicator") 
 
 if __name__ == '__main__':
     main()
