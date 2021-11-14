a00b6d629c258133d1d2387065a3d768487cb3d7
Dhruv patel <dhruvpatel1057@gmail.com>
2021-11-14 09:36:37 -0600

    version 1.0 complete

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ import datetime as dt
 files = os.listdir('preprocessed')
 print(files)
 
-companies = ['Bank of America', 'Capital One', 'JP Morgan Chase', 'Goldman Sachs',  'Morgan Stanley']
+companies = ['Bank of America', 'Capital One', 'Goldman Sachs', 'JP Morgan Chase', 'Morgan Stanley']
 
 # def calc_change(predicted_val, previous_close):
 #     return (predicted_val - previous_close) / 100
@@ -22,11 +22,20 @@ def main():
 
     st.sidebar.title('Select Asset Type')
 
-    options = ["Top picks", "Stocks", "Crypto"]
+    options = ["Top_picks", "Stocks", "Crypto"]
 
     choice = st.sidebar.selectbox("Select one", options)
+
+    if choice == "Top_picks":
+
+        st.subheader("Our Top 3 Stocks for Today")
+        df_top = pd.read_csv('output/top_three.csv',index_col=0)
+        st.dataframe(df_top)
     
     if choice == "Stocks":
+        
+        st.subheader("Stock Analytics")
+
         for i in range(len(files)):
             try:
                 df = pd.read_csv(f"preprocessed/{files[i]}")
@@ -35,12 +44,41 @@ def main():
                 st.dataframe(stats)
                 selected = st.checkbox('Show graph', key = companies[i])
                 if (selected):
-                    fig  = px.line(df, x="Date", y="Close",title=companies[i])
+                    
+                    df1= df[:len(df)-90]
+                    # preds = df[len(df)-90]
+                    print("***",df1)
+
+                    rem = df[len(df)-90:]
+                    
+                #     fig = go.Figure(
+                #         data=[go.Candlestick(x=df['Date'],
+                # # open=df['AA'],
+                # # high=df['AAPL.High'],
+                # # low=df['AAPL.Low'],
+                # close=df['Close'])]),
+                    
+
+                    fig  = px.line(df1, x="Date", y="Close",title=companies[i])
+                    # fig.add_scatter(x=preds['Date'], y=preds['Close'], mode='markers')
+                    # fig.add_scatter(x=rem['Date'], y=rem['Close'])
+
+                    # px.line(preds,x="Date",y="Close"    )
+                    # (px.line(preds, x="Date", y="Close"))
                     st.plotly_chart(fig, use_container_width=True)
+                show_data = st.checkbox('Show data',key = companies[i])
+                if(show_data):
+                    
+                    st.dataframe(df)
+                # if st.checkbox("show raw data: ",False):
+                #     st.dataframe(df);
             except:
                 print('Error')
+                print(files[i])
 
-    # if choice == "Top picks":
+    if choice == "Crypto":
+
+        st.subheader("Coming Soon.....")
     # Print the top 3 recommended stocks
 
         # st.metric('Temperature', 42, -2.56)
