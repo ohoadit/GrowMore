256870b500d98d5be96824caf63f256863a0169c
ohoadit <aditshah8@gmail.com>
2021-11-14 03:53:52 -0600

    add template for displaying the stocks

--- app.py
+++ app.py
@@ -1,32 +1,38 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import os
+import plotly.graph_objects as go
 from dataset import *
 
+files = os.listdir('preprocessed')
 
+print(files)
+
+companies = ['Capital One', 'Bank of America', 'JP Morgan Chase', 'Goldman Sachs', 'Morgan Stanley']
+
+def calc_change(predicted_val, previous_close):
+    return (predicted_val - previous_close) / 100
 
 def main():
 
     st.sidebar.title('Select Asset Type')
 
-
-
     options = ["Stocks","Crypto"]
 
     choice = st.sidebar.selectbox("Select one", options)
-    # st.subheader("Subheader")
     
-
     if choice == "Stocks":
-
-        st.subheader("Stock name")
-        st.subheader("plot")
-        st.subheader("checkbox for techical indicator")
-
-        
-
-
-
+        for i in range(len(files)):
+            df = pd.read_csv(f"preprocessed/{files[i]}")
+            st.subheader(companies[i])
+            fig = go.Figure(go.Candlestick(x=df.index,
+            close=df['Close']))
+            fig.show()
+            selected = st.checkbox('Show graph', key = companies[i])
+            if (selected):
+                st.table(df[:20])
+        st.metric('Temperature', 42, -2.56)
 
 if __name__ == '__main__':
     main()
